ea0f022048e7bb4dc8a0d6611b321b6dddf117ec
alexstaringgit <alexstaring425@gmail.com>
2023-09-08 18:34:42 +0200

    Make a cover page

--- landingpage/views.py
+++ landingpage/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "landingpage/index.html")
+
+
+def projects(request):
+    return render(request, "landingpage/projects.html")
--- mysite/settings.py
+++ mysite/settings.py
@@ -34,14 +34,15 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bootstrap5",
     "landingpage",
+    "bootstrap5",
+    "debug_toolbar"
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -52,6 +53,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "mysite.urls"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 TEMPLATES = [
     {
