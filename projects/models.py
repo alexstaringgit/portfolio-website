55dad2a39ee93ef54e0572dbf1f05fb0eca4379f
alexstaringgit <alexstaring425@gmail.com>
2023-09-09 19:22:30 +0200

    Update contents of each project being displayed

--- projects/models.py
+++ projects/models.py
@@ -4,4 +4,10 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
+    description = models.TextField("Project description", default="")
+    link = models.CharField(max_length=200, default="")
+    tasks = models.TextField("Challenges of that project", default="")
     completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Project: " + self.title
